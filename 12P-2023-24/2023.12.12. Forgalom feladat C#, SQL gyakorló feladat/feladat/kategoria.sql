4311b7c31897314297a7661f109efca6a78dd074
cerberus2477 <tabor.tunde@gmail.com>
2025-02-10 22:19:05 +0100

    2023.12.06. Mikulás animált digitális képeslap jön

---
+++ 12P-2023-24/2023.12.12. Forgalom feladat C#, SQL gyakorló feladat/feladat/kategoria.sql
@@ -0,0 +1,7 @@
+﻿create table kategoria (kat_kod int,kat_nev varchar(50));
+insert into kategoria values (4,'Édesség');
+insert into kategoria values (1,'Húsáru');
+insert into kategoria values (3,'Tejtermék');
+insert into kategoria values (5,'Tészták');
+insert into kategoria values (6,'Üditőitalok');
+insert into kategoria values (2,'Zöldség');
